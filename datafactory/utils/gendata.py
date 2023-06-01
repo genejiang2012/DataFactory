201d0ac0e841469c422e6f673c40b017618d137a
genejiang2012 <genejiang2012@outlook.com>
2023-06-01 09:51:17 +0800

    update the gendata files

--- datafactory/utils/gendata.py
+++ datafactory/utils/gendata.py
@@ -16,22 +16,24 @@ from datafactory.common.setting import get_yaml
 
 class DataGenerator:
     def __init__(self, faker, meta, connect=None):
-        self.log = log
-        self.all_packages = {}
-        self.pre_data = {}
-        self.env_data = {'faker': faker, 'env': {}}
-        self.result_data = {}
-        self.extraction_data = {}
-        self.error_fields = {}
         self.faker = faker
-        self.sql = []
-        # self.db = Database(connect) if connect else None
         self.meta_data = get_yaml(meta)
+        self.db = None
+        self.all_packages = {}
+        self.log = log
+        self.env_data = {'faker': faker, 'env': {}}
+        self.field_data = {}
+        self.extraction_data = {}
+        self.log = log
+        self.sql = []
+        self.error_data = {}
 
     def start(self):
-        self.result_data = {}
-        self.extraction_data = {}
-        self.env()
+        self._env()
+        # self._tables_handle()
+        # self.extraction()
+        # self._error_handle()
+        # self.data2sql()
 
     def _env(self):
         """
@@ -43,6 +45,56 @@ class DataGenerator:
         if not env:
             return
         self._field_handle(env_key='env', **env)
+
+    def _field_handle(self, env_key=None, max_number=1, **kwargs):
+        result = []
+        i = 0
+        while i < max_number:
+            data = {}
+            for key, value in copy.deepcopy(kwargs).items():
+                try:
+                    _value = self.dict_resolve(value)
+
+    def dict_resolve(self, data: dict):
+        """
+        递归将字典的value使用模板格式化
+        :param data:
+        :return:
+        """
+        if not isinstance(data, dict) and isinstance(data, str):
+            return self._template_render(data)
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                if isinstance(value, dict):
+                    self.dict_resolve(value)
+                elif isinstance(value, str):
+                    value = self._template_render(value)
+                data[key] = value
+        return data
+
+
+    def _template_render(self, s, env=None):
+        """
+        jinja2模板渲染
+        :param s:
+        :param env:
+        :return:
+        """
+        source_type = type(s)
+
+        if isinstance(s, (list, dict)):
+            s = json.dumps(s).replace('\\\"', "'") # dict to string
+        tp = jinja2.Template(s)
+
+        if isinstance(env, dict):
+            self.all_packages.update(env)
+        tp.globals.update(self.all_packages)
+
+        r = tp.render(**self.env_data)
+
+        if source_type in [list, dict]:
+            r = json.loads(r)   # json to dict
+        return r
 
     def _gen_data(self, **kwargs):
         """
@@ -74,28 +126,7 @@ class DataGenerator:
                 raise Exception('rule type must be dictionary or list!')
         return r
 
-    def _template_render(self, s, env=None):
-        """
-        jinja2模板渲染
-        :param s:
-        :param env:
-        :return:
-        """
-        source_type = type(s)
 
-        if isinstance(s, (list, dict)):
-            s = json.dumps(s).replace('\\\"', "'")
-        tp = jinja2.Template(s)
-        if isinstance(env, dict):
-            self.all_packages.update(env)
-
-        tp.globals.update(self.all_packages)
-
-        r = tp.render(**self.env_data)
-
-        if source_type in [list, dict]:
-            r = json.loads(r)
-        return r
 
 
 if __name__ == '__main__':
